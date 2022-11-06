39fe2f729ec1bf8ec6d2e993cf30c2ac9e25c4e8
BobanCvetanoski <117604259+BobanCvetanoski@users.noreply.github.com>
2022-11-06 23:35:49 +0100

    Create zadaca02.java

---
+++ zadaca02.java
@@ -0,0 +1,34 @@
+class Vraboten {
+    public String Ime;
+    public String Prezime;
+    public String Plata;
+    public Vraboten(String ime, String prezime, String plata) {
+        Ime = ime;
+        Prezime = prezime;
+        Plata = plata;
+    }
+    public String getIme() {
+        return Ime;
+    }
+    public void setIme(String ime) {
+        this.Ime = Ime;
+    }
+    public String getPrezime() {
+        return Prezime;
+    }
+    public void setPrezime(String prezime) {
+        this.Prezime = Prezime;
+    }
+    public String getPlata() {
+        return Plata;
+    }
+    public void setPlata(String plata) {
+        Plata = plata;
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Vraboten nov = new Vraboten("Boban" ,"Cvetanoski" , "77777");
+        System.out.println("Imeto na vraboteniot e: " + nov.Ime + "\n" + "Prezimeto na vraboteniot e: " + nov.Prezime + "\n" + "Platata na vraboteniot iznesuva:  " + nov.Plata );
+    }
+}
